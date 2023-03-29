d510557fb74d66c3332267a9a75c7f7ace68f11b
prettyblueberry <prettyblueberry52702@gmail.com>
2023-03-29 12:05:30 +0900

    -match the customized functions into socket.js -completed it finally.

--- server/socket.js
+++ server/socket.js
@@ -2,14 +2,22 @@
 export default (server) => {
     const io = require('socket.io').listen(server)
     io.on('connection', function(socket){
+        socket.emit('yourSid', socket.id);
         io.emit('createCube', socket.id);
-        socket.on('move', function(data){
-            io.emit('move', socket.id, data);
+        socket.on('move', function(buttonKey, moveDistance, rotateAngle){
+            io.emit('move', socket.id, buttonKey, moveDistance, rotateAngle);
         })
         socket.on('disconnect', function(){
             console.log('disconnected!!!');
             io.emit('remove', socket.id);
         })
+        socket.broadcast.emit("askPosition", socket.id);
+
+        socket.on("answerPosition", function(sid, position, color){
+            console.log(position);
+            socket.to(sid).emit('answerPosition', socket.id, position, color);
+        })
     });
+    
 }
 
