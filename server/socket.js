6d67459fc9296108d8310c9315319a57fa539e0c
prettyblueberry <prettyblueberry52702@gmail.com>
2023-03-29 10:25:06 +0900

    -link socket.io (create, remove, move)

--- server/socket.js
+++ server/socket.js
@@ -2,7 +2,14 @@
 export default (server) => {
     const io = require('socket.io').listen(server)
     io.on('connection', function(socket){
-        console.log(socket.id);
+        io.emit('createCube', socket.id);
+        socket.on('move', function(data){
+            io.emit('move', socket.id, data);
+        })
+        socket.on('disconnect', function(){
+            console.log('disconnected!!!');
+            io.emit('remove', socket.id);
+        })
     });
 }
 
